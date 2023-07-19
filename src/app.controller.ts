c2a6ba2301e70328d5e78fe271ad89574fa541d9
Andrei Savelyeu1 <andrei_savelyeu1@epam.com>
2023-07-19 17:03:12 +0300

    added validation for body for creation endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,30 +8,36 @@ import {
   Body,
   HttpCode,
   ParseUUIDPipe,
-  ParseEnumPipe
+  ParseEnumPipe,
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Report, ReportType } from './data';
+import { CreateReportDto } from './dtos/report.dto';
 
 @Controller('report/:type')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getAllReports(@Param('type', new ParseEnumPipe(ReportType)) type: ReportType): Report[] {
+  getAllReports(
+    @Param('type', new ParseEnumPipe(ReportType)) type: ReportType,
+  ): Report[] {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
     return this.appService.getAllReports(reportType);
   }
 
   @Get(':id')
-  getReportById(@Param('id', ParseUUIDPipe) id: string, @Param('type', new ParseEnumPipe(ReportType)) type: ReportType) {
+  getReportById(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Param('type', new ParseEnumPipe(ReportType)) type: ReportType,
+  ) {
     return this.appService.getReportById(id, type);
   }
 
   @Post()
   createReport(
-    @Body() body: { source: string; amount: number },
+    @Body() body: CreateReportDto,
     @Param('type', new ParseEnumPipe(ReportType)) type: ReportType,
   ) {
     const reportType =
