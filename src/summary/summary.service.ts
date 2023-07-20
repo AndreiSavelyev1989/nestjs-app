b16b548b187e1e7b9f24e3c8dfd6d140fd300eb1
Andrei Savelyeu1 <andrei_savelyeu1@epam.com>
2023-07-20 13:39:56 +0300

    added endpoint for calculation the amount of reports

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { ReportType } from 'src/data';
 import { ReportService } from 'src/report/report.service';
 
 @Injectable()
@@ -6,6 +7,24 @@ export class SummaryService {
   constructor(private readonly reportService: ReportService) {}
 
   calculateSummary() {
+    const totalIncome = this.reportService
+      .getAllReports(ReportType.INCOME)
+      .reduce((acc, report) => {
+        acc += report.amount;
+        return acc;
+      }, 0);
 
+    const totalExpense = this.reportService
+      .getAllReports(ReportType.EXPENSE)
+      .reduce((acc, report) => {
+        acc += report.amount;
+        return acc;
+      }, 0);
+
+    return {
+      totalIncome,
+      totalExpense,
+      netIncome: totalIncome - totalExpense,
+    };
   }
 }
